6775a5a5661dabb68414a5485e6f58e1c66dc15a
lirundong <lrd.001@163.com>
2020-07-03 17:15:07 +0800

    :tada: init commit, private tool

---
+++ get_clash_cfg.py
@@ -0,0 +1,80 @@
+"""Load custom rules and update Clash configuration from subscription URL.
+
+Custom rule format:
+
+..code:: YAML
+    key1: val1
+    key2: val2
+    ...
+    rules:
+        - name: custom_rule_name_1
+          proxies:
+            - proxy_1
+            - Proxy_2
+            - ...
+          rule:
+            - rule_1
+            - rule_2
+            - ...
+        - name: custom_rule_2
+            ...
+"""
+
+import requests
+import yaml
+
+PG_T = "Proxy Group"
+RULE_T = "Rule"
+
+
+def get_subscription(url, **params):
+    r = requests.get(url, params=params, headers={"user-agent": "clash"})
+    if r.status_code != 200:
+        raise requests.HTTPError(r.reason)
+    sub_info = yaml.load(r.text, Loader=yaml.SafeLoader)
+    return sub_info
+
+
+def get_custom_rule(path):
+    info = yaml.load(open(path, "r", encoding="utf-8"), Loader=yaml.SafeLoader)
+    rules = info["rules"]
+    info.pop("rules")
+    return rules, info
+
+
+def update_with_custom_rule(sub, custom_rules, **custom_kv):
+    assert all(k in sub for k in custom_kv.keys())
+
+    new_pg = []
+    new_rules = []
+    for rule in custom_rules:
+        pg = {
+            "name": rule["name"],
+            "type": "select",
+            "proxies": rule["proxies"],
+        }
+        new_pg.append(pg)
+        for r in rule["rule"]:
+            r += f",{rule['name']}"
+            new_rules.append(r)
+    
+    sub.update(custom_kv)
+    sub[PG_T] = new_pg + sub[PG_T]
+    sub[RULE_T] = new_rules + sub[RULE_T]
+
+    return sub
+
+
+if __name__ == "__main__":
+    # V2fly, private
+    url = "https://sub.v2club.top/api/v1/client/subscribe"
+    params = {"token": "90cc293a40e07f4387655fdf6722225f"}
+    custom_path = "my_rules.yaml"
+    output_path = "V2fly.yaml"
+
+    sub = get_subscription(url, **params)
+    custom_rules, custom_cfg = get_custom_rule(custom_path)
+    updated_sub = update_with_custom_rule(sub, custom_rules, **custom_cfg)
+    open(output_path, "w", encoding="utf-8").write(
+        yaml.dump(updated_sub, Dumper=yaml.SafeDumper, allow_unicode=True)
+    )
