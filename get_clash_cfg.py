e6c117274dfd671d675df05cc1f9db2042e57bfb
lirundong <lird@shanghaitech.edu.cn>
2020-07-19 19:09:58 +0800

    allow add custom proxies to specific proxy groups

--- get_clash_cfg.py
+++ get_clash_cfg.py
@@ -6,6 +6,10 @@ Custom rule format:
     key1: val1
     key2: val2
     ...
+    proxies:
+      - [in clash proxy format]
+    add-to-group: # add previous `proxies' to which proxy groups
+      - proxy group names
     rules:
         - name: custom_rule_name_1
           proxies:
@@ -20,11 +24,14 @@ Custom rule format:
             ...
 """
 
+from itertools import chain
+
 import requests
 import yaml
 
 PG_T = "proxy-groups"
 RULE_T = "rules"
+PROXY_T = "proxies"
 
 
 def get_subscription(url, **kwargs):
@@ -39,10 +46,14 @@ def get_custom_rule(path):
     info = yaml.load(open(path, "r", encoding="utf-8"), Loader=yaml.SafeLoader)
     rules = info["rules"]
     info.pop("rules")
-    return rules, info
+    proxies = info["proxies"]
+    info.pop("proxies")
+    add_to = info["add-to-group"]
+    info.pop("add-to-group")
+    return rules, proxies, add_to, info
 
 
-def update_with_custom_rule(sub, custom_rules, **custom_kv):
+def update_with_custom_rule(sub, custom_rules, custom_proxies, add_to_pg, **custom_kv):
     assert all(k in sub for k in custom_kv.keys())
 
     new_pg = []
@@ -60,9 +71,18 @@ def update_with_custom_rule(sub, custom_rules, **custom_kv):
             r += f",{rule['name']}"
             new_rules.append(r)
     
+    # update proxy groups
+    proxy_groups = []
+    custom_proxy_names = [p["name"] for p in custom_proxies]
+    for pg in chain(new_pg, sub[PG_T]):
+        if pg["name"] in add_to_pg:
+            pg["proxies"] = custom_proxy_names + pg["proxies"]
+        proxy_groups.append(pg)
+    
     sub.update(custom_kv)
-    sub[PG_T] = new_pg + sub[PG_T]
+    sub[PG_T] = proxy_groups
     sub[RULE_T] = new_rules + sub[RULE_T]
+    sub[PROXY_T] = custom_proxies + sub[PROXY_T]
 
     return sub
 
@@ -82,8 +102,8 @@ if __name__ == "__main__":
     output_path = "V2fly.yaml"
 
     sub = get_subscription(url, **sub_args)
-    custom_rules, custom_cfg = get_custom_rule(custom_path)
-    updated_sub = update_with_custom_rule(sub, custom_rules, **custom_cfg)
+    custom_rules, custom_proxies, proxy_add_to, custom_cfg = get_custom_rule(custom_path)
+    updated_sub = update_with_custom_rule(sub, custom_rules, custom_proxies, proxy_add_to, **custom_cfg)
     open(output_path, "w", encoding="utf-8").write(
         yaml.dump(updated_sub, Dumper=yaml.SafeDumper, allow_unicode=True)
     )
