fb21d4b448ae94fe97ada15edf65fc393fa7e066
lirundong <lird@shanghaitech.edu.cn>
2020-10-01 01:21:20 +0800

    add complete local ips

--- setup_gateway_iptables.sh
+++ setup_gateway_iptables.sh
@@ -9,11 +9,22 @@ REDIR_PORT="10082"
 DNS_PORT="10053"
 TPROXY_MARKER="0x01"
 ROUTING_TABLE="100"
-LOCAL_IP="198.18.0.0/16"
+LOCAL_IPS=(
+  "0.0.0.0/8"
+  "10.0.0.0/8"
+  "127.0.0.0/8"
+  "169.254.0.0/16"
+  "172.16.0.0/12"
+  "192.168.0.0/16"
+  "224.0.0.0/4"
+  "240.0.0.0/4"
+)
 
 # Let's start with mangle table:
 # 1. Do not touch UDP packets which destinate to other LAN devices.
-iptables -t mangle -A PREROUTING -d ${LOCAL_IP} -p udp -j RETURN
+for LOCAL_IP in ${LOCAL_IPS}; do
+  iptables -t mangle -A PREROUTING -d ${LOCAL_IP} -p udp -j RETURN
+done
 # 2. Do not touch DNS packets, as they will be handled by nat rules later.
 iptables -t mangle -A PREROUTING -p udp -m udp --dport 53 -j RETURN
 # 3. Redirect all other UDP packets to a localhost socket which is listening on 
@@ -33,6 +44,8 @@ ip route add local default dev lo table ${ROUTING_TABLE}
 iptalbes -t nat -A PREROUTING -p udp -m udp --dport 53 \
   -j REDIRECT --to-ports ${DNS_PORT}
 # 2. Do not touch TCP packets which destinate to other LAN devices.
-iptalbes -t nat -A PREROUTING -d ${LOCAL_IP} -p tcp -j RETURN
+for LOCAL_IP in ${LOCAL_IPS}; do
+  iptalbes -t nat -A PREROUTING -d ${LOCAL_IP} -p tcp -j RETURN
+done
 # 3. Finally redirect all other TCP packets to REDIR_PORT on localhost.
 iptables -t nat -A PREROUTING -p tcp -j REDIRECT --to-ports ${REDIR_PORT}
