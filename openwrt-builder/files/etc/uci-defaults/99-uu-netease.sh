bd7ac2760536f60118dc7c08ef386aade1b13e49
lirundong <lrd.001@163.com>
2022-04-18 13:38:30 +0800

    [openwrt-builder] Use oneliner for uu plugin install.

--- openwrt-builder/files/etc/uci-defaults/99-uu-netease.sh
+++ openwrt-builder/files/etc/uci-defaults/99-uu-netease.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-/usr/bin/curl -sSL http://uu.gdl.netease.com/uuplugin-script/202012111056/install.sh -o /tmp/install.sh
-sh /tmp/install.sh openwrt $(uname -m)
-rm /tmp/install.sh
+/usr/bin/curl -sSL http://uu.gdl.netease.com/uuplugin-script/202012111056/install.sh | sh -s -- openwrt $(uname -m)
