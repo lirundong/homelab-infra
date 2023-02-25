536712994529a29379754498ea468edb2272c265
lirundong <lird@shanghaitech.edu.cn>
2023-02-25 22:48:55 +0800

    [util] Upgrade to TencentCloud APIv3

--- util-cookbook/tencent-cloud/register_dns.py
+++ util-cookbook/tencent-cloud/register_dns.py
@@ -3,16 +3,16 @@
 # Copy to /usr/bin/update_qcloud_dns on OpenWRT.
 
 from argparse import ArgumentParser
-import base64
 import copy
+from datetime import datetime, timezone
+import hashlib
 import hmac
+import json
 import os
 from pathlib import Path
 from pprint import pprint
-import random
 import sys
 from typing import Dict, Tuple, List
-import time
 import warnings
 
 # TODO: Remove this path hack.
@@ -22,36 +22,29 @@ import requests
 
 from common import secrets
 
-QCLOUD_API_HOSTNAME = "cns.api.qcloud.com"
-QCLOUD_API_PATH = "/v2/index.php"
+QCLOUD_API_HOSTNAME = "dnspod.tencentcloudapi.com"
 QCLOUD_PUBLIC_API = {
-    "Action": None,
-    "Region": None,
-    "Timestamp": None,
-    "Nonce": None,
-    "SecretId": None,
-    "Signature": None,
-    "SignatureMethod": None,
-    "Token": None,
+    "X-TC-Action": None,
+    "X-TC-Region": None,
+    "X-TC-Timestamp": None,
+    "X-TC-Token": None,
+    "X-TC-Language": None,
 }
 QCLOUD_DNS_API = {
-    "RecordModify": {
-        "domain": None,
-        "recordId": None,
-        "subDomain": None,
-        "recordType": None,
-        "recordLine": None,
-        "value": None,
-        "ttl": None,
-        "mx": None,
+    "DescribeRecordList": {
+        "X-TC-Version": "2021-03-23",
+        "Domain": None,
+        "Subdomain": None,
     },
-    "RecordList": {
-        "domain": None,
-        "offset": None,
-        "length": None,
-        "subDomain": None,
-        "recordType": None,
-        "qProjectId": None,
+    "ModifyRecord": {
+        "X-TC-Version": "2021-03-23",
+        "Domain": None,
+        "Subdomain": None,
+        "RecordType": None,
+        "RecordLine": None,
+        "Value": None,
+        "RecordId": None,
+        "MX": None,
     },
 }
 
@@ -79,88 +72,133 @@ def normalize_dict(src: Dict) -> Dict:
     return dst
 
 
-def get_qcloud_signature(
-    plain_request: Dict[str, str],
-    secret_key: str,
-    request_method: str = "GET",
-    hmac_method: str = "sha256",
-) -> str:
-    ordered_request = []
-    for key in sorted(plain_request.keys()):
-        val = plain_request[key]
-        if val is None:
-            raise ValueError(f"Get None value for request key {key}")
-        key = key.replace("_", ".")
-        ordered_request.append(f"{key}={val}")
-    request_plain_str = f"{request_method}{QCLOUD_API_HOSTNAME}{QCLOUD_API_PATH}?" + "&".join(
-        ordered_request
+def get_qcloud_auth(headers: Dict[str, str], payload: Dict[str, str]) -> str:
+    # 0. Protect the inputs.
+    headers = copy.copy(headers)
+    payload = copy.copy(payload)
+    # 1. Build the CanonicalRequest.
+    canonical_request_template = (
+        r"{http_request_method}"
+        "\n"
+        r"{canonical_uri}"
+        "\n"
+        r"{canonical_query_string}"
+        "\n"
+        r"{canonical_headers}"
+        "\n"
+        r"{signed_headers}"
+        "\n"
+        r"{hashed_request_payload}"
     )
-    signature = hmac.new(
-        key=secret_key.encode("utf-8"),
-        msg=request_plain_str.encode("utf-8"),
-        digestmod=hmac_method,
+    headers.setdefault("Host", QCLOUD_API_HOSTNAME)
+    headers.setdefault("Content-Type", "application/json")
+    sorted_headers = [
+        (k.lower().strip(), headers[k].lower().strip()) for k in sorted(list(headers.keys()))
+    ]
+    hashed_request_payload = hashlib.sha256(json.dumps(payload).encode("utf-8")).hexdigest()
+    canonical_request = canonical_request_template.format(
+        http_request_method="POST",
+        canonical_uri="/",
+        canonical_query_string="",
+        canonical_headers="\n".join(f"{h[0]}:{h[1]}" for h in sorted_headers) + "\n",
+        signed_headers=";".join(h[0] for h in sorted_headers),
+        hashed_request_payload=hashed_request_payload,
+    )
+    # 2. Build the StringToSign.
+    string_to_sign_template = (
+        r"{algorithm}"
+        "\n"
+        r"{request_timestamp}"
+        "\n"
+        r"{date}/{service}/tc3_request"  # CredentialScope
+        "\n"
+        r"{hashed_canonical_request}"
+    )
+    timestamp = int(headers["X-TC-Timestamp"])
+    date = datetime.fromtimestamp(timestamp, tz=timezone.utc).strftime("%Y-%m-%d")
+    service = "dnspod"
+    hashed_canonical_request = hashlib.sha256(canonical_request.encode("utf-8")).hexdigest()
+    string_to_sign = string_to_sign_template.format(
+        algorithm="TC3-HMAC-SHA256",
+        request_timestamp=timestamp,
+        date=date,
+        service=service,
+        hashed_canonical_request=hashed_canonical_request,
+    )
+    # 3. Compute the Signature.
+    secret_key = secrets.QCLOUD_API_SECRET_KEY
+    secret_date = hmac.new(
+        key=f"TC3{secret_key}".encode("utf-8"),
+        msg=date.encode("utf-8"),
+        digestmod="sha256",
     ).digest()
-    encoded_signature = base64.b64encode(signature).decode("utf-8")
-
-    return encoded_signature
-
-
-def get_full_request(
-    action: str,
-    action_request: Dict[str, str],
-    request_method: str = "GET",
-    hmac_method: str = "sha256",
-) -> Dict[str, str]:
-    assert request_method in ("GET", "PUT")
-    assert hmac_method in ("sha256", "sha1")
-
-    qcloud_secret_id, qcloud_secret_key = (
-        secrets.QCLOUD_API_SECRET_ID,
-        secrets.QCLOUD_API_SECRET_KEY,
+    secret_service = hmac.new(
+        key=secret_date,
+        msg=service.encode("utf-8"),
+        digestmod="sha256",
+    ).digest()
+    secret_signing = hmac.new(
+        key=secret_service,
+        msg="tc3_request".encode("utf-8"),
+        digestmod="sha256",
+    ).digest()
+    signature = hmac.new(
+        key=secret_signing,
+        msg=string_to_sign.encode("utf-8"),
+        digestmod="sha256",
+    ).hexdigest()
+    # 4. Build the final Authorization.
+    authorization_template = (
+        r"{algorithm} "
+        r"Credential={secret_id}/{date}/{service}/tc3_request, "
+        r"SignedHeaders={signed_headers}, "
+        r"Signature={signature}"
     )
-    request = copy.copy(QCLOUD_PUBLIC_API)
-    request["Action"] = action
-    request["Timestamp"] = int(time.time())
-    request["Nonce"] = random.randint(0, 1 << 31)
-    request["SecretId"] = qcloud_secret_id
-    request["SignatureMethod"] = "HmacSHA256" if hmac_method == "sha256" else "HmacSHA1"
-    request.update(action_request)
-    request = normalize_dict(request)
-    request["Signature"] = get_qcloud_signature(
-        request, qcloud_secret_key, request_method, hmac_method
+    secret_id = secrets.QCLOUD_API_SECRET_ID
+    authorization = authorization_template.format(
+        algorithm="TC3-HMAC-SHA256",
+        secret_id=secret_id,
+        date=date,
+        service=service,
+        signed_headers=";".join(h[0] for h in sorted_headers),
+        signature=signature,
     )
+    return authorization
 
-    return request
 
-
-def get_qcloud_subdomain_record(
-    domain: str,
-    subdomain: str,
-    request_method: str = "GET",
-    hmac_method: str = "sha256",
-) -> List[Dict[str, str]]:
+def get_qcloud_subdomain_record(domain: str, subdomain: str) -> List[Dict[str, str]]:
     """Get subdomain's record id and previous value, return None if it does not exist."""
-    request = copy.copy(QCLOUD_DNS_API["RecordList"])
-    request["domain"] = domain
-    request["subDomain"] = subdomain
-    request = get_full_request("RecordList", request, request_method, hmac_method)
-    response = requests.get(
-        f"https://{QCLOUD_API_HOSTNAME}{QCLOUD_API_PATH}", params=request
-    ).json()
+    payload = copy.copy(QCLOUD_DNS_API["DescribeRecordList"])
+    payload["Domain"] = domain
+    payload["Subdomain"] = subdomain
+    payload = normalize_dict(payload)
 
-    if response["code"] != 0:
-        raise RuntimeError(response["message"])
+    headers = copy.copy(QCLOUD_PUBLIC_API)
+    headers["X-TC-Action"] = "DescribeRecordList"
+    headers["X-TC-Version"] = payload.pop("X-TC-Version")
+    headers["X-TC-Timestamp"] = str(int(datetime.now(timezone.utc).timestamp()))
+    headers = normalize_dict(headers)
+    headers["Authorization"] = get_qcloud_auth(headers, payload)
+
+    response = requests.post(
+        f"https://{QCLOUD_API_HOSTNAME}", json=payload, headers=headers
+    ).json()["Response"]
+
+    if response.get("Error"):
+        err_code = response["Error"]["Code"]
+        err_msg = response["Error"]["Message"]
+        raise RuntimeError(f"{err_code}: {err_msg}")
     ret = []
-    for record in response["data"]["records"]:
-        assert record["name"] == subdomain
+    for record in response["RecordList"]:
+        assert record["Name"] == subdomain
         filtered_record = {
-            "recordId": record["id"],
-            "recordType": record["type"],
-            "recordLine": record["line"],
-            "value": record["value"],
+            "RecordId": record["RecordId"],
+            "RecordType": record["Type"],
+            "RecordLine": record["Line"],
+            "Value": record["Value"],
         }
-        if record["type"] == "MX":
-            filtered_record["mx"] = record["mx"]
+        if record["MX"]:
+            filtered_record["MX"] = record["MX"]
         ret.append(filtered_record)
 
     return ret
@@ -169,26 +207,24 @@ def get_qcloud_subdomain_record(
 def update_qcloud_subdomain_record(
     domain: str,
     subdomain: str,
-    request_method: str = "GET",
-    hmac_method: str = "sha256",
     get_ip_method: str = "ipify",
     dry_run: bool = False,
 ):
-    current_records = get_qcloud_subdomain_record(domain, subdomain, request_method, hmac_method)
+    current_records = get_qcloud_subdomain_record(domain, subdomain)
     ipv4, ipv6 = get_public_ip_addresses(get_ip_method)
 
     updating_records = []
     for record in current_records:
-        if record["recordType"] == "A" and record["value"] == ipv4:
+        if record["RecordType"] == "A" and record["Value"] == ipv4:
             continue
-        if record["recordType"] == "AAAA":
-            if (ipv6 is None) or (ipv6 and record["value"] == ipv6):
+        if record["RecordType"] == "AAAA":
+            if (ipv6 is None) or (ipv6 and record["Value"] == ipv6):
                 continue
-        update_record = copy.copy(QCLOUD_DNS_API["RecordModify"])
-        update_record["domain"] = domain
-        update_record["subDomain"] = subdomain
+        update_record = copy.copy(QCLOUD_DNS_API["ModifyRecord"])
+        update_record["Domain"] = domain
+        update_record["SubDomain"] = subdomain
         update_record.update(record)
-        update_record["value"] = ipv4 if record["recordType"] == "A" else ipv6
+        update_record["Value"] = ipv4 if record["RecordType"] == "A" else ipv6
         update_record = normalize_dict(update_record)
         updating_records.append(update_record)
 
@@ -197,15 +233,24 @@ def update_qcloud_subdomain_record(
         pprint(updating_records)
     else:
         new_records = []
-        for request in updating_records:
-            request = get_full_request("RecordModify", request, request_method, hmac_method)
-            response = requests.get(
-                f"https://{QCLOUD_API_HOSTNAME}{QCLOUD_API_PATH}", params=request
-            ).json()
-            if response["code"] != 0:
-                raise RuntimeError(response["message"])
+        for payload in updating_records:
+            headers = copy.copy(QCLOUD_PUBLIC_API)
+            headers["X-TC-Action"] = "ModifyRecord"
+            headers["X-TC-Version"] = payload.pop("X-TC-Version")
+            headers["X-TC-Timestamp"] = str(int(datetime.now(timezone.utc).timestamp()))
+            headers = normalize_dict(headers)
+            headers["Authorization"] = get_qcloud_auth(headers, payload)
+
+            response = requests.post(
+                f"https://{QCLOUD_API_HOSTNAME}", json=payload, headers=headers
+            ).json()["Response"]
+
+            if response.get("Error"):
+                err_code = response["Error"]["Code"]
+                err_msg = response["Error"]["Message"]
+                raise RuntimeError(f"{err_code}: {err_msg}")
             else:
-                new_records.append(response["data"]["record"])
+                new_records.append(payload)
         print("Updated records:")
         pprint(new_records)
 
@@ -214,8 +259,6 @@ if __name__ == "__main__":
     parser = ArgumentParser("Update QCloud DNS records by host IP addresses.")
     parser.add_argument("--domain", "-d", help="Domain to be updated.")
     parser.add_argument("--sub-domain", "-s", help="Sub-domain to be updated.")
-    parser.add_argument("--request-method", default="GET", help="Method to call QCloud API.")
-    parser.add_argument("--hmac-method", default="sha256", help="Method to hash QCloud signature.")
     parser.add_argument(
         "--get-ip-method", default="ipify", help="Method to get host public IP addresses."
     )
@@ -230,8 +273,6 @@ if __name__ == "__main__":
     update_qcloud_subdomain_record(
         args.domain,
         args.sub_domain,
-        args.request_method,
-        args.hmac_method,
         args.get_ip_method,
         args.dry_run,
     )
