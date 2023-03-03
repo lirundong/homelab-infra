daf838cf845855f80b27a6984ed82d5f3d1c38d9
lirundong <lird@shanghaitech.edu.cn>
2023-03-03 20:31:13 +0800

    [openwrt] Add Plex rules to firewall

--- openwrt-builder/files/etc/uci-defaults/99-firewall.sh
+++ openwrt-builder/files/etc/uci-defaults/99-firewall.sh
@@ -30,6 +30,17 @@ uci set firewall.$rule_id.proto='tcp'
 uci set firewall.$rule_id.family='ipv6'
 uci set firewall.$rule_id.target='ACCEPT'
 
+# Allow Plex forward from WAN to synonas via IPv6.
+rule_id=$(uci add firewall rule)
+uci set firewall.$rule_id.name='Allow-Plex@synonas-WAN-LAN'
+uci set firewall.$rule_id.src='wan'
+uci set firewall.$rule_id.dest='lan'
+uci set firewall.$rule_id.dest_port='32400'
+uci set firewall.$rule_id.proto='tcp'
+uci set firewall.$rule_id.proto='udp'
+uci set firewall.$rule_id.family='ipv6'
+uci set firewall.$rule_id.target='ACCEPT'
+
 # Forward synonas web services.
 rule_id=$(uci add firewall redirect)
 uci set firewall.$rule_id.name='DNAT-services@synonas-WAN-LAN'
@@ -65,6 +76,19 @@ uci set firewall.$rule_id.dest='lan'
 uci set firewall.$rule_id.dest_ip='192.168.50.6'
 uci set firewall.$rule_id.dest_port='28996'
 uci set firewall.$rule_id.proto='tcp'
+uci set firewall.$rule_id.family='ipv4'
+uci set firewall.$rule_id.target='DNAT'
+
+# Forward synonas Plex.
+rule_id=$(uci add firewall redirect)
+uci set firewall.$rule_id.name='DNAT-Plex@synonas-WAN-LAN'
+uci set firewall.$rule_id.src='wan'
+uci set firewall.$rule_id.src_dport='32400'
+uci set firewall.$rule_id.dest='lan'
+uci set firewall.$rule_id.dest_ip='192.168.50.6'
+uci set firewall.$rule_id.dest_port='32400'
+uci set firewall.$rule_id.proto='tcp'
+uci set firewall.$rule_id.proto='udp'
 uci set firewall.$rule_id.family='ipv4'
 uci set firewall.$rule_id.target='DNAT'
 
