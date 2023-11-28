f31471747697ef1c7aebc5591397bf4a6e73f479
lirundong <lird@shanghaitech.edu.cn>
2023-11-28 18:57:14 +0800

    [confgen] revert GeoIP no-resolve in clash, add smart homekit domains

--- conf-gen/rule/ir.py
+++ conf-gen/rule/ir.py
@@ -54,10 +54,6 @@ class GeoIP(IRBase):
     _clash_prefix = "GEOIP"
     _quantumult_prefix = "geoip"
 
-    @property
-    def clash_rule(self):
-        return f"{super().clash_rule},no-resolve"
-
 
 @_IR_REGISTRY.register()
 class IPCIDR(IRBase):
