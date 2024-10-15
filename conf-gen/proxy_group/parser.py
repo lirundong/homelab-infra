66c720e10c157a322514c7f9ac9a00bffc0c15be
Rundong Li <rundong.001@gmail.com>
2024-10-15 13:42:26 +0800

    [conf-gen] refine region name grouping, add FlyBird proxies

--- conf-gen/proxy_group/parser.py
+++ conf-gen/proxy_group/parser.py
@@ -27,14 +27,21 @@ def merge_proxy_by_region(
         elif re.search(stat_proxy_name_pattern, proxy.name, re.IGNORECASE):
             proxies_by_region["📈 Statistics"].append(proxy)
         else:
-            region_flags = emoji.emoji_list(proxy.name)
-            if len(region_flags) != 1:
-                raise ValueError(f"Cannot recognize country info from {proxy.name}")
-            region_flag = region_flags[0]["emoji"]
-            region_info = pycountry.countries.search_fuzzy(region_flag)[0]
-            # Taiwan requires special handling.
+            if region_flags := emoji.emoji_list(proxy.name):
+                search_key = region_flags[0]["emoji"]
+            else:
+                search_key = proxy.name.split("-")[0]
+                # Hardcode fix for FlyingBird proxies.
+                if search_key.lower() == "turkey":
+                    search_key = "Turkiye"
+                elif search_key.lower() == "uk":
+                    search_key = "United Kingdom"
+            region_info = pycountry.countries.search_fuzzy(search_key)[0]
+            # Hardcode fix for Taiwan proxies with China flag in their names.
             if region_info.name == "China" and re.search(r"Taiwan", proxy.name, re.IGNORECASE):
-                region_name = "Taiwan"
+                region_info = pycountry.countries.get(common_name="Taiwan")
+            if hasattr(region_info, "common_name"):
+                region_name = region_info.common_name
             else:
                 region_name = region_info.name
             proxies_by_region[f"{region_info.flag} {region_name}"].append(proxy)
