61d829e63fa6afbc5696876a5eb95672251c8da0
Rundong Li <rundong.001@gmail.com>
2026-06-28 18:18:25 +0800

    [openwrt-builder] disable root password SSH auth

--- openwrt-builder/files/etc/uci-defaults/99-dropbear.sh
+++ openwrt-builder/files/etc/uci-defaults/99-dropbear.sh
@@ -4,6 +4,7 @@ uci -q batch << EOI
     set dropbear.@dropbear[0].Port='22'
     set dropbear.@dropbear[0].Interface='lan'
     set dropbear.@dropbear[0].PasswordAuth='off'
+    set dropbear.@dropbear[0].RootPasswordAuth='off'
 
     commit dropbear
 EOI
